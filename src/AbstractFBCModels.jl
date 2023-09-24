1a4d7abb254f9379a019e59e006dea2920089e91
Mirek Kratochvil <exa.exa@gmail.com>
2023-09-24 15:47:53 +0200

    remove requriedInterfaces (good effort but it is impractical here)

--- src/AbstractFBCModels.jl
+++ src/AbstractFBCModels.jl
@@ -5,53 +5,10 @@ module AbstractFBCModels
 
 using DocStringExtensions
 
-import RequiredInterfaces as R
-
 include("types.jl")
 include("accessors.jl")
 include("io.jl")
-
-# utility functions used to read models, common to many file types
 include("utils.jl")
-
-# define required interface for FBCModel types
-R.@required AbstractFBCModel begin
-    # IO
-    # load(::Type{JSONFBCModel}, ::String) # TODO RequiredInterfaces.jl issue #13
-    save(::AbstractFBCModel, ::String)
-    # filename_extensions(::Type{JSONFBCModel}) # TODO RequiredInterfaces.jl issue #13
-    # basic accessors
-    reactions(::AbstractFBCModel)
-    n_reactions(::AbstractFBCModel)
-    metabolites(::AbstractFBCModel)
-    n_metabolites(::AbstractFBCModel)
-    genes(::AbstractFBCModel)
-    n_genes(::AbstractFBCModel)
-    # optimization problem
-    stoichiometry(::AbstractFBCModel)
-    bounds(::AbstractFBCModel)
-    balance(::AbstractFBCModel)
-    objective(::AbstractFBCModel)
-    # reaction annotations
-    reaction_gene_associations(::AbstractFBCModel, ::String)
-    reaction_stoichiometry(::AbstractFBCModel, ::String)
-    reaction_annotations(::AbstractFBCModel, ::String)
-    reaction_notes(::AbstractFBCModel, ::String)
-    reaction_name(::AbstractFBCModel, ::String)
-    # metabolite annotations
-    metabolite_formula(::AbstractFBCModel, ::String)
-    metabolite_charge(::AbstractFBCModel, ::String)
-    metabolite_compartment(::AbstractFBCModel, ::String)
-    metabolite_annotations(::AbstractFBCModel, ::String)
-    metabolite_notes(::AbstractFBCModel, ::String)
-    metabolite_name(::AbstractFBCModel, ::String)
-    # gene annotations    
-    gene_annotations(::AbstractFBCModel, ::String)
-    gene_notes(::AbstractFBCModel, ::String)
-    gene_name(::AbstractFBCModel, ::String)
-    # conversion
-    # convert(::Type{JSONFBCModel}, model::AbstractFBCModel) # TODO RequiredInterfaces.jl issue #13
-end
 
 export load,
     save,
