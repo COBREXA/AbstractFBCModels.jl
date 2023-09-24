deef55b1e4b555293e6c66d17e12914a0555e101
Mirek Kratochvil <exa.exa@gmail.com>
2023-09-24 15:49:45 +0200

    eradicate exports
    "Low-level" packages shouldn't export anything because "end users" shouldn't
    ever be required to touch this directly.

--- src/AbstractFBCModels.jl
+++ src/AbstractFBCModels.jl
@@ -10,32 +10,4 @@ include("accessors.jl")
 include("io.jl")
 include("utils.jl")
 
-export load,
-    save,
-    filename_extensions,
-    reactions,
-    n_reactions,
-    metabolites,
-    n_metabolites,
-    genes,
-    n_genes,
-    stoichiometry,
-    bounds,
-    balance,
-    objective,
-    reaction_gene_associations,
-    reaction_stoichiometry,
-    reaction_annotations,
-    reaction_notes,
-    reaction_name,
-    metabolite_formula,
-    metabolite_charge,
-    metabolite_compartment,
-    metabolite_annotations,
-    metabolite_notes,
-    metabolite_name,
-    gene_annotations,
-    gene_notes,
-    gene_name
-
 end # module AbstractFBCModel
