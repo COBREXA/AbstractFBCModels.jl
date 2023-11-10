7b35cbfb8d5982e6500f99a2ae54b36d1fa70a4e
Mirek Kratochvil <exa.exa@gmail.com>
2023-11-10 10:21:57 +0100

    add better docs to downloads

--- src/utils.jl
+++ src/utils.jl
@@ -39,6 +39,8 @@ end
 $(TYPEDSIGNATURES)
 
 Check if the file at `path` has the expected hash.
+
+At this point, the hash is always SHA256.
 """
 function check_cached_file_hash(path, expected_checksum)
     actual_checksum = bytes2hex(sha256(open(path)))
@@ -53,7 +55,7 @@ $(TYPEDSIGNATURES)
 
 Download the file at `url` and save it at `path`, also check if this file is
 the expected file by calling [`check_cached_file_hash`](@ref). If the file has
-already been downloaded, and stored at `path`, then it is not downloaded again.
+already been downloaded and stored at `path`, then it is not downloaded again.
 """
 function download_data_file(url, path, hash)
     if isfile(path)
