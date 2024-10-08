b67a6e50f9212e550de0f5102f568d4bca21adf6
Documenter.jl <documenter@juliadocs.github.io>
2024-10-08 09:43:06 +0000

    build based on f7d4a2b

--- dev/utilities.jl
+++ dev/utilities.jl
@@ -14,6 +14,8 @@ A.accessors()
 
 @test length(A.accessors()) == 35 #src
 
+@test length(A.required_accessors()) == 11 #src
+
 # You do not need to overload all of them (e.g., if you model does not have any
 # genes you can completely omit all gene-related functions). The main required
 # ones (esp. the reaction- and metabolite-related ones) will throw an error if
